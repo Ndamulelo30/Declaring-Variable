fdfea66a9a1f6e0058153afeb676b58cce124162
Ndamulelo30 <141257791+Ndamulelo30@users.noreply.github.com>
2024-02-16 13:56:33 +0200

    Add files via upload

---
+++ 7.DeclaringVariable/src/main/java/com/mycompany/declaringvariable/App.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.declaringvariable;
+
+/**
+ *
+ * @author Sanele
+ */
+public class App {
+
+    public static void main(String[] args) {
+        int myNum = 5;
+        float myFloatNum = 5.99f;
+        char myLetter = 'D';
+        boolean myBool = true;
+        String myText = "Nyaluvhani";
+        System.out.println(myNum);
+        System.out.println(myFloatNum); 
+        System.out.println(myLetter);
+        System.out.println(myBool);
+        System.out.println(myText);
+    }
+}
---
+++ 7.DeclaringVariables/src/main/java/com/mycompany/declaringvariables/App.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.declaringvariables;
+
+/**
+ *
+ * @author Sanele
+ */
+public class App {
+
+    public static void main(String[] args) {
+        String name = "Tshifhiwa";
+        System.out.println(name);
+    }
+}
